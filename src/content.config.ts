a6dab9d788c741f2fb03a72932fe982c6689466b
Repooc <tjcooper1982@gmail.com>
2025-01-17 03:05:07 -0500

    remove under construction banner

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,12 +4,12 @@ import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema({
-		extend: z.object({
-		  // Add a default value to the built-in `banner` field.
-		  banner: z.object({ content: z.string() }).default({
-			content: 'The site is currently under construction.',
-		  }),
-		}),
+		// extend: z.object({
+		//   // Add a default value to the built-in `banner` field.
+		//   banner: z.object({ content: z.string() }).default({
+		// 	content: 'The site is currently under construction.',
+		//   }),
+		// }),
 	  })
 	}),
 };
