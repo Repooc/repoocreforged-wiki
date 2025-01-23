2f185e9b2770e6b90fee7c322905e9987b7130de
Repooc <tjcooper1982@gmail.com>
2025-01-23 17:30:57 -0500

    formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,54 +7,52 @@ import starlightImageZoom from "starlight-image-zoom";
 
 // https://astro.build/config
 export default defineConfig({
-    redirects: {
-        "/addons/actionbarbuddy": "/elvui-plugins/actionbarbuddy",
-        "/addons/actionbarmasks": "/elvui-plugins/actionbarmasks",
-        "/addons/dynamicstatusicons": "/elvui-plugins/dynamicstatusicons",
-        "/addons/funstuff": "/elvui-plugins/funstuff",
-        "/addons/keybinddisplay": "/elvui-plugins/keybinddisplay",
-        "/addons/ratiominimapauras": "/elvui-plugins/ratiominimapauras",
-        "/addons/scrapsupport": "/elvui-plugins/scrapsupport",
-        "/addons/splitactionbars": "/elvui-plugins/splitactionbars",
-        "/addons/tooltiptweaks": "/elvui-plugins/tooltiptweaks",
-    },
-    integrations: [
-        starlight({
-            plugins: [starlightImageZoom()],
-            components: {
-                Pagination: "./src/components/Pagination.astro",
-                SocialIcons: "./src/components/SocialIcons.astro",
+  redirects: {
+    "/addons/actionbarbuddy": "/elvui-plugins/actionbarbuddy",
+    "/addons/actionbarmasks": "/elvui-plugins/actionbarmasks",
+    "/addons/dynamicstatusicons": "/elvui-plugins/dynamicstatusicons",
+    "/addons/funstuff": "/elvui-plugins/funstuff",
+    "/addons/keybinddisplay": "/elvui-plugins/keybinddisplay",
+    "/addons/ratiominimapauras": "/elvui-plugins/ratiominimapauras",
+    "/addons/scrapsupport": "/elvui-plugins/scrapsupport",
+    "/addons/splitactionbars": "/elvui-plugins/splitactionbars",
+    "/addons/tooltiptweaks": "/elvui-plugins/tooltiptweaks",
+  },
+  integrations: [
+    starlight({
+      plugins: [starlightImageZoom()],
+      components: {
+        Pagination: "./src/components/Pagination.astro",
+        SocialIcons: "./src/components/SocialIcons.astro",
+      },
+      title: "Repooc Reforged",
+      editLink: {
+        baseUrl: "https://github.com/repooc/repoocreforged-wiki/edit/preview/",
+      },
+      lastUpdated: true,
+      logo: { src: "/src/assets/HighResLogo.webp" },
+      customCss: ["./src/styles/custom.css", "./src/fonts/font-face.css"],
+      social: {
+        discord: "https://repoocreforged.dev/discord",
+        github: "https://github.com/repooc",
+        patreon: "https://patreon.com/repoocreforged",
+      },
+      sidebar: [
+        {
+          label: "Getting Started",
+          items: [
+            {
+              label: "Introduction",
+              slug: "introduction",
             },
-            title: "Repooc Reforged",
-            editLink: {
-                baseUrl:
-                    "https://github.com/repooc/repoocreforged-wiki/edit/preview/",
-            },
-            lastUpdated: true,
-            logo: { src: "/src/assets/HighResLogo.webp" },
-            customCss: ["./src/styles/custom.css", "./src/fonts/font-face.css"],
-            social: {
-                discord: "https://repoocreforged.dev/discord",
-                github: "https://github.com/repooc",
-                patreon: "https://patreon.com/repoocreforged",
-            },
-            sidebar: [
-                {
-                    label: "Getting Started",
-                    items: [
-                        // Each item here is one entry in the navigation menu.
-                        {
-                            label: "Introduction",
-                            slug: "introduction",
-                        },
-                    ],
-                },
-                {
-                    label: "ElvUI Plugins",
-                    autogenerate: { directory: "elvui-plugins" },
-                },
-            ],
-        }),
-        mdx(),
-    ],
+          ],
+        },
+        {
+          label: "ElvUI Plugins",
+          autogenerate: { directory: "elvui-plugins" },
+        },
+      ],
+    }),
+    mdx(),
+  ],
 });
