edfac68c6323408c8ee52dedd0de640df7e67ded
Repooc <tjcooper1982@gmail.com>
2026-04-30 19:36:39 -0400

    remove this

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,6 @@ import starlightImageZoom from "starlight-image-zoom";
 
 // https://astro.build/config
 export default defineConfig({
-  redirects: {
-    "/addons/actionbarbuddy": "/elvui-plugins/actionbarbuddy",
-    "/addons/actionbarmasks": "/elvui-plugins/actionbarmasks",
-    "/addons/dynamicstatusicons": "/elvui-plugins/dynamicstatusicons",
-    "/addons/funstuff": "/elvui-plugins/funstuff",
-    "/addons/keybinddisplay": "/elvui-plugins/keybinddisplay",
-    "/addons/ratiominimapauras": "/elvui-plugins/ratiominimapauras",
-    "/addons/scrapsupport": "/elvui-plugins/scrapsupport",
-    "/addons/splitactionbars": "/elvui-plugins/splitactionbars",
-    "/addons/tooltiptweaks": "/elvui-plugins/tooltiptweaks",
-  },
   integrations: [
     starlight({
       plugins: [starlightImageZoom()],
